efcb568a4904f852336ff720d6712faaf68e6039
ufukbakan <println.ufukbakan@gmail.com>
2023-04-10 17:42:19 +0300

    feat: add & save actual data

--- src/components/RecordAdder.tsx
+++ src/components/RecordAdder.tsx
@@ -8,31 +8,38 @@ import { AppPasswordRecord } from "./AppPassword";
 import AppPicker from './AppPicker';
 import { DialogProps } from './Dialog';
 
-interface RecordAdderProps extends Partial<DialogProps>{
+interface RecordAdderProps extends Partial<Omit<DialogProps, "close">>, Pick<DialogProps, "close"> {
     onAdd: (app: AppPasswordRecord) => void
 }
 
 export default function (props: RecordAdderProps) {
     const [showPassword, setShowPassword] = useState(false);
     const [selectedApp, setSelectedApp] = useState<AppPasswordRecord["app"]>("Binance");
+    const [recordName, setRecordName] = useState("");
+    const [recordPassword, setRecordPassword] = useState("");
+
+    function onAdd(){
+        props.onAdd({
+            app: selectedApp,
+            name: recordName,
+            password: recordPassword
+        })
+        props.close();
+    }
 
     return (
         <>
             <AppPicker value={selectedApp} onChange={(d) => setSelectedApp(d)} />
             <Vstack style={{ marginBottom: 50 }}>
-                <TextInput style={globalStyles.input} placeholder='Kayıt ismi giriniz' />
+                <TextInput style={globalStyles.input} placeholder='Kayıt ismi giriniz' value={recordName} onChange={(e) => setRecordName(e.nativeEvent.text)} />
                 <Hstack style={{ alignItems: "center", gap: 10 }}>
-                    <TextInput style={globalStyles.input} placeholder='Şifre giriniz' secureTextEntry={!showPassword} />
+                    <TextInput style={globalStyles.input} placeholder='Şifre giriniz' secureTextEntry={!showPassword} value={recordPassword} onChange={(e) => setRecordPassword(e.nativeEvent.text)} />
                     <Ionicons name={showPassword ? "eye-off" : "eye"} size={24} color="#444" onPress={() => setShowPassword(p => !p)} />
                 </Hstack>
             </Vstack>
             <Hstack style={{ width: "100%", justifyContent: "space-around" }}>
                 <Button title="Kapat" type='solid' color="error" onPress={props.close} />
-                <Button title="Ekle" type='solid' color="success" onPress={() => props.onAdd({
-                    app: selectedApp,
-                    name: "Kayıt ismi",
-                    password: "Şifre"
-                })} />
+                <Button title="Ekle" type='solid' color="success" onPress={onAdd} />
             </Hstack>
         </>
     )
--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext } from "react";
 import { AppPasswordRecord } from "src/components/AppPassword";
+import { saveData } from "./storage-utils";
 
 export type ContextValue = {
     records: AppPasswordRecord[];
@@ -17,6 +18,10 @@ var MyContext = React.createContext(initialValues);
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
     var [value, setValue] = React.useState(initialValues.get());
+
+    useEffect(() => {
+        saveData("storedPasswords", value.records);
+    }, [value.records]);
 
     return (
         <MyContext.Provider value={{ get: () => value, set: setValue }}>
